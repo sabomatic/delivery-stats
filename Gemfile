e6afbc202875bd4f22930585953912bb990747d8
Bootcamp <bootcamp@Sab.local>
2017-02-13 10:22:23 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+#third party libraries
+gem 'minitest', '~> 5.4'
+gem 'minitest-reporters'
+
+#gemfiles
+gem 'mail', '~> 2.6'
+gem 'guard'
+gem 'guard-minitest'
---
+++ test/test_mail_parser.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative '../lib/mail_parser'
+
+class MailParserTest < Minitest::Test
+  def test_something
+  end
+end
