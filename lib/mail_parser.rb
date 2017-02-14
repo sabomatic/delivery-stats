0e72024884c5bb67cd21f8a6351948100adfcdb0
Bootcamp <bootcamp@Sab.local>
2017-02-14 11:45:33 +0100

    refactored passgerman email

--- lib/mail_parser.rb
+++ lib/mail_parser.rb
@@ -1,12 +1,31 @@
 require 'mail'
+require 'pry'
+require 'cgi'
 
 
 class MailParser
   def self.parse(email)
-  mail = Mail.read('test/email.eml')
-  body = mail.parts[0].body
-  stringbody = body.to_s
+    mail = Mail.read(email)
+    body = mail.parts[0].body.raw_source
+    body_string = body.to_s
 
-  string_array = stringbody.split(/\n/)
+    string_array = clean_body(body_string)
 
+    index = string_array.index('Kundeninformation')
+    index_2 = string_array.index('Total in CHF')+1
+
+    customer_hash = {
+      date: string_array[index-1].split(' ')[1],
+      window: string_array[index-1].split(' ')[2],
+      name: string_array[index+1],
+      street: string_array[index+3],
+      zip: string_array[index+4],
+      total: string_array[index_2]
+    }
   end
+
+  def self.clean_body(body_string)
+    body_string = body_string.gsub('=', '%').gsub("%20", '').gsub('> ', '').gsub("\r", '')
+    CGI::unescape(body_string).split(/\n/)
+  end
+end
--- test/test_mail_parser.rb
+++ test/test_mail_parser.rb
@@ -5,9 +5,9 @@ Minitest::Reporters.use!
 require_relative '../lib/mail_parser'
 
 class MailParserTest < Minitest::Test
-  def test_something
+  def test_german_email
     customer_hash = {
-      date: '23.01.2017',
+      date: '23.01.17',
       window: '19:00-19:30',
       name: 'Christine Mika',
       street: 'Bürglistrasse 26',
@@ -15,6 +15,21 @@ class MailParserTest < Minitest::Test
       total: '53,00'
     }
 
-assert_equal customer_hash, MailParser.parse('test/email.eml')
+assert_equal customer_hash, MailParser.parse('test/fixtures/email_1.eml')
     end
+
+
+    def test_english_email
+      skip
+      customer_hash = {
+        date: '12.01.17',
+        window: '19:00-19:30',
+        name: 'Mona Moschko',
+        street: 'Sihlhallenstrasse 11',
+        zip: '8004',
+        total: '59.20'
+      }
+
+  assert_equal customer_hash, MailParser.parse('test/fixtures/email_2.eml')
+      end
 end
